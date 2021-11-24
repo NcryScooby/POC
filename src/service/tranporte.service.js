61d494bf01fef11bf7db64cc7e50978edf61bbcf
FabricioCaldana <fanelli@grupodimed.com.br>
2021-11-24 17:40:39 -0300

    teste

--- src/service/tranporte.service.js
+++ src/service/tranporte.service.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export async function getCoords(id) {
-    return await axios.get(`http://www.poatransporte.com.br/php/facades/process.php?a=il&p=${id}`);
+    return await axios.get(`https://www.poatransporte.com.br/php/facades/process.php?a=il&p=${id}`);
 }
 
 export async function getOnibus(tipoBus) {
-    return await axios.get(`http://www.poatransporte.com.br/php/facades/process.php?a=nc&p=%&t=${tipoBus}`);
+    return await axios.get(`https://www.poatransporte.com.br/php/facades/process.php?a=nc&p=%&t=${tipoBus}`);
 }
