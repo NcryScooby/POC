c6bfb7df2a3f89e0f27110100516cd4ae6a6e8a0
FabricioCaldana <fabricio_c_anelli@hotmail.com>
2021-12-01 13:59:39 -0300

    melhorando responsividade

--- src/app/ui/style.js
+++ src/app/ui/style.js
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 
 export const SubTitle = styled.h2`
-  color: white;
-  font-weight: 400;
+  color: #ccc;
+  font-weight: 600;
   font-size: 1rem;
-  font-family: "Roboto", sans-serif;
+  font-family: 'Montserrat', sans-serif;
 `;
 
 export const Select = styled.div`
@@ -20,6 +20,8 @@ export const Select = styled.div`
     outline: none;
     border-radius: 10px;
     cursor: pointer;
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 500;
   }
 
   @media (min-width: 601px) and (max-width: 766px) {
@@ -50,64 +52,64 @@ export const Select = styled.div`
 `;
 
 export const Title = styled.h1`
-  font-family: "Roboto", sans-serif;
-  font-weight: 300;
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 400;
   color: #fff;
   font-size: 1.3em;
 
   /* Moto G4, Galaxy S5 */
   @media (min-height: 639px) and (max-height: 641px) {
-    font-size: 1em;
+    font-size: 0.91em;
   }
 
   /* Pixel 2 */
   @media (min-height: 730px) and (max-height: 732px) {
-    font-size: 1.2em;
+    font-size: 1.04em;
   }
 
   /* Pixel 2 XL*/
   @media (min-height: 822px) and (max-height: 824px) {
-    font-size: 1.2em;
+    font-size: 1.04em;
   }
 
   /* Iphone 5, SE */
   @media (min-height: 567px) and (max-height: 569px) {
-    font-size: 0.9em;
+    font-size: 0.8em;
   }
 
   /* Iphone 6, 7, 8 */
   @media (min-height: 666px) and (max-height: 668px) {
-    font-size: 1.1em;
+    font-size: 0.95em;
   }
 
   /* Iphone 6+, 7+, 8+ */
   @media (min-height: 735px) and (max-height: 737px) {
-    font-size: 1.2em;
+    font-size: 1.05em;
   }
 
   /* Iphone X */
   @media (min-height: 811px) and (max-height: 813px) {
-    font-size: 1.1em;
+    font-size: 0.95em;
   }
 
   /* Ipad */
   @media (min-height: 1023px) and (max-height: 1025px) {
-    font-size: 1.8em;
+    font-size: 1.99em;
   }
 
   /* Ipad pro*/
   @media (min-height: 1365px) and (max-height: 1367px) {
-    font-size: 2em;
+    font-size: 2.67em;
   }
 
   /* Surface Duo */
   @media (min-height: 719px) and (max-height: 721px) {
-    font-size: 1.2em;
+    font-size: 1.38em;
   }
 
   /* Galaxy Fold */
   @media (min-height: 652px) and (max-height: 654px) {
-    font-size: 0.8em;
+    font-size: 0.69em;
   }
 `;
 
@@ -118,56 +120,56 @@ export const Mapa = styled.div`
 
   /* Moto G4, Galaxy S5 */
   @media (min-height: 639px) and (max-height: 641px) {
-    height: 58vh;
+    height: 59vh;
   }
 
   /* Pixel 2 */
   @media (min-height: 730px) and (max-height: 732px) {
-    height: 61vh;
+    height: 63vh;
   }
 
   /* Pixel 2 XL*/
   @media (min-height: 822px) and (max-height: 824px) {
-    height: 65vh;
+    height: 67vh;
   }
 
   /* Iphone 5, SE */
   @media (min-height: 567px) and (max-height: 569px) {
-    height: 54vh;
+    height: 55vh;
   }
 
   /* Iphone 6, 7, 8 */
   @media (min-height: 666px) and (max-height: 668px) {
-    height: 58vh;
+    height: 60vh;
   }
 
   /* Iphone 6+, 7+, 8+ */
   @media (min-height: 735px) and (max-height: 737px) {
-    height: 61vh;
+    height: 62vh;
   }
 
   /* Iphone X */
   @media (min-height: 811px) and (max-height: 813px) {
-    height: 66vh;
+    height: 67vh;
   }
 
   /* Ipad */
   @media (min-height: 1023px) and (max-height: 1025px) {
-    height: 68vh;
+    height: 66vh;
   }
 
   /* Ipad pro*/
   @media (min-height: 1365px) and (max-height: 1367px) {
-    height: 75vh;
+    height: 71vh;
   }
 
   /* Surface Duo */
   @media (min-height: 719px) and (max-height: 721px) {
-    height: 60vh;
+    height: 58vh;
   }
 
   /* Galaxy Fold */
   @media (min-height: 652px) and (max-height: 654px) {
-    height: 61vh;
+    height: 62vh;
   }
 `;
