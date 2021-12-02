fa8003e01beb27ae39e733c840d5d7ef15d7e334
FabricioCaldana <fanelli@grupodimed.com.br>
2021-12-02 13:09:31 -0300

    Adicionando valor no teste

--- cypress/integration/map.spec.js
+++ cypress/integration/map.spec.js
@@ -3,11 +3,11 @@
 describe('Map', () => {
     it ('Ônibus', () => {
         cy.visit('http://localhost:3000/');
-        cy.get('[data-cy=opcoes]').select('Ônibus');
+        cy.get('[data-cy=opcoes]').select('Ônibus').should('have.value', 'Ônibus');
     });
 
     it ('Lotação', () => {
         cy.visit('http://localhost:3000/');
-        cy.get('[data-cy=opcoes]').select('Lotação');
-    });
+        cy.get('[data-cy=opcoes]').select('Lotação').should('have.value', 'Lotação');
+    });        
 });
